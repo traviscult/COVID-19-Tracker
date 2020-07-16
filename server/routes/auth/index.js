b402726c8b59811dc6d0cec0a923ec302503eb0b
Joy Chen <joychen5069@gmail.com>
2020-07-15 20:48:39 -0400

    pushing for debugging

--- server/routes/auth/index.js
+++ server/routes/auth/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const passport = require('../../passport');
-const userController = require("../../controllers/userController");
+// const userController = require("../../controllers/userController");
 
 // this route is just used to get the user basic info
 router.get('/user', userController.getUser)
