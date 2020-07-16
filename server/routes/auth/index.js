a534ca96a7ad6b63f98ee578d84af97d6b3c1c6d
Joy Chen <joychen5069@gmail.com>
2020-07-15 21:04:05 -0400

    fixed new user function

--- server/routes/auth/index.js
+++ server/routes/auth/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const passport = require('../../passport');
 // const userController = require("../../controllers/userController");
+const userController = require('../../contollers/userController')
 
 // this route is just used to get the user basic info
 router.get('/user', userController.getUser)
