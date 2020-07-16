122ab3b8760283dfae43918e1359f3c6b2125641
traviscult <60018617+traviscult@users.noreply.github.com>
2020-07-15 21:07:32 -0400

    added axios call

---
+++ server/utils/API.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+  axios.get('https://api.infermedica.com/covid19/diagnosis', {
+    headers: {
+        "App-Id": 'f3ec44a2', 
+        "App-Key": 'ea2e478aeea48ed264c553324544937e' 
+    }
+ })
+
+  
