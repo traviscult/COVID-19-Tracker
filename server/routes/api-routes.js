e65390eb45087974e6b1a5b6822301364b9c3f7d
Joy Chen <joychen5069@gmail.com>
2020-07-18 13:00:24 -0400

    updated api routes

--- server/routes/api-routes.js
+++ server/routes/api-routes.js
@@ -4,7 +4,7 @@ const authRoutes = require("./auth");
 
 router.use('/auth', authRoutes);
 
-router.get("/", (req, res) => {
+router.get("/members", (req, res) => {
   res.send(` <div style="color: brown; text-align: center"> <h1> Server API Router is working! </h1><p>This is coming from server port 3001. The client folder will serve actual UI display where React is running on port 3000. <br />
   This is for router testing purpose only! </div> `);
 });
