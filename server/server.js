b9ae83f7cecdb99945a680f50c94dcf12a7cc9a9
traviscult <60018617+traviscult@users.noreply.github.com>
2020-07-25 12:11:15 -0400

    serve up static url in server js

--- server/server.js
+++ server/server.js
@@ -26,6 +26,10 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
 app.use(routes)
 
 app.listen(PORT, (req, res) => {
