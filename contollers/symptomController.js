f031666f5d4987be1977eea0289e48a65ee9bb2a
Shi Sha <emailtotheowner@gmail.com>
2020-07-28 15:28:34 -0400

    Pulling new updates

--- contollers/symptomController.js
+++ contollers/symptomController.js
@@ -1,48 +1,48 @@
-const axios = require ("axios");
+const axios = require("axios");
 const db = require("../models");
 
 
 module.exports = {
-    questions: function(res){
-        axios.get('https://api.infermedica.com/covid19/symptoms', {
-            headers: {
-                "App-Id": 'f3ec44a2', 
-                "App-Key": 'ea2e478aeea48ed264c553324544937e' 
-            }
-         }).then(response => {
-          //  console.log("response", response)
-          //  console.log("response data", response.data)
-         }) 
-    },
-    questionsPost: function(res){
-        axios({
-            url: 'https://api.infermedica.com/covid19/diagnosis',
-            method: 'POST',
-            headers: {
-              'App-Id': 'f3ec44a2',
-              'App-Key': 'ea2e478aeea48ed264c553324544937e'
-              //   'Content-Type': 'application/json'
-            },
-            data: {
-              sex: 'male',
-              age: 30,
-              evidence: []
-            }
-          })
-            .then(response => {
-              // response.data.headers['Content-Type'];
-              //     console.log("headers", response.data.headers)
-              //    console.log("response2", response)
-              // console.log('response2 data', response.data);
-              // console.log(response.data.question.items)
-              return res.json(response.data)
-            })
-            .catch(function (error) {
-              console.log(error);
-              //   console.log('Show error notification!');
-            });
-                 
-}
+  questions: function (res) {
+    axios.get('https://api.infermedica.com/covid19/symptoms', {
+      headers: {
+        "App-Id": 'f3ec44a2',
+        "App-Key": 'ea2e478aeea48ed264c553324544937e'
+      }
+    }).then(response => {
+      //  console.log("response", response)
+      //  console.log("response data", response.data)
+    })
+  },
+  questionsPost: function (res) {
+    axios.post({
+      url: 'https://api.infermedica.com/covid19/diagnosis',
+      method: 'POST',
+      headers: {
+        'App-Id': 'f3ec44a2',
+        'App-Key': 'ea2e478aeea48ed264c553324544937e'
+        //   'Content-Type': 'application/json'
+      },
+      data: {
+        sex: 'male',
+        age: 30,
+        evidence: []
+      }
+    })
+      .then(response => {
+        // response.data.headers['Content-Type'];
+        //     console.log("headers", response.data.headers)
+        //    console.log("response2", response)
+        // console.log('response2 data', response.data);
+        // console.log(response.data.question.items)
+        return res.json(response.data)
+      })
+      .catch(function (error) {
+        console.log(error);
+        //   console.log('Show error notification!');
+      });
+
+  }
 }
 
 
