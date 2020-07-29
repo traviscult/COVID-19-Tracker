3b4777a3abddf60561fe619bc22c34b821e3ab19
Cate Dunn <60521577+CateDunn@users.noreply.github.com>
2020-07-29 14:28:51 -0400

    droptdown pulling county name correctly

--- client/src/components/members/CountyDropdown.jsx
+++ client/src/components/members/CountyDropdown.jsx
@@ -3,16 +3,39 @@ import React from 'react';
 
 
 export default class CountyDropdown extends React.Component{
+    constructor(){
+        super();
+        this.state ={value: '' };
 
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange (event) {
+        this.setState({value: event.target.value});
+    }
+
+   handleSubmit = (event) => {
+        alert('You have chosen the county of ' + this.state.value)
+        event.preventDefault();
+    }
+  
     render(){
         let counties = this.props.state.counties
         let countyoptions = counties.map((county) =>
         <option key={county}>{county}</option>);
         return (
             <div>
-                <select>
-                    {countyoptions}
-                </select>
+                <form onSubmit={this.handleSubmit}>
+                    <label> Choose a County: <br />
+                        <select value={this.state.value} onChange={this.handleChange}>
+                        <option value="" selected="defaultValue">Select from here...</option>
+                            {countyoptions}
+                        </select>
+                            <input type="submit" value="Submit" className="stateSubmitBtn" ></input>
+                    </label> 
+                </form>
+                
            </div>
         )
     }
