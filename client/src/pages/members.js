bac3027b16167ce0e0d360cfbc7738ff631431aa
traviscult <60018617+traviscult@users.noreply.github.com>
2020-08-01 13:41:10 -0400

    added disclosure

--- client/src/pages/members.js
+++ client/src/pages/members.js
@@ -13,7 +13,6 @@ const Members = (props) => {
         <Header />
         <Navbar logout={props.logout} />
         <h3 className="text-center pageTitle">Welcome to members dashboard!</h3>
-
         <div className="container">
           <div className="row">
             <div className="col-sm-12 col-md-5">
@@ -23,6 +22,13 @@ const Members = (props) => {
               <StateSearch />
             </div>
           </div>
+          <div className="text-center">
+            <h5>Disclosure</h5>
+        <p>Please be advised the Covid sympotm checker is not a diagnosis, it is for informational purposes only and does not represent, in any way, a qualified medical opinion. </p>
+        <p>The symptom checker and its results are entirely based on WHO and CDC guidelines concerning COVID-19 only.</p>
+        <p>If this is an emergency, call your local emergency number immediately. Do not proceed with the symptom checker. Medical attention is required immediately.</p>
+        <p>Your data is safe. Information that you provide is anonymous and not shared with anyone.</p>
+        </div>
         </div>
         <Footer />
       </div>
