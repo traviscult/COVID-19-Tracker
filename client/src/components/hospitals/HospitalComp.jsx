5403e9a9e15f0c92428fbd254c91789512e96b7b
Joy Chen <joychen5069@gmail.com>
2020-08-13 10:43:44 -0400

    added map feature (broken. fixing bugs)

---
+++ client/src/components/hospitals/HospitalComp.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import './hospital.css';
+import axios from "axios";
+
+//const dotenv = require('dotenv');
+//const env = dotenv.config().parsed;
+
+export default class HospitalsPage extends React.Component {
+//   state = {
+//     source: [],
+//     title: [],
+//     author: [],
+//     url: []
+//   }
+  
+
+  componentDidMount() {
+//     mapboxgl.accessToken = 'pk.eyJ1Ijoiam95Y2hlbjUwNjkiLCJhIjoiY2tkc3ZsMWp0MG1hMTJybnhpNW9wbDh3NyJ9.7jiZP4oIoTlXI--KIOjT5A'
+
+//     const map = new mapboxgl.Map({
+//         container: 'map',
+//         style: 'mapbox://styles/mapbox/streets-v11',
+//         center: [-77.04, 38.907],
+//         });
+  };
+
+  render() {
+    return (
+      <>
+        <div id="map">map goes here</div>
+      </>
+    )
+  }
+}
---
+++ client/src/pages/Hospitals.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Header from "../components/header/Header";
+import Footer from "../components/footer/Footer";
+import Navbar from "../components/navigation/Navbar";
+import HospitalComp from "../components/hospitals/HospitalComp";
+import "../components/hospitals/hospital.css";
+
+const HospitalsPage = (props) => {
+  return (
+    <>
+      <div className="container mainWrapper">
+        <Header />
+        <Navbar logout={props.logout} />
+        {/* <HospitalComp /> */}
+        <Footer />
+      </div>
+    </>
+  );
+};
+export default HospitalsPage;
