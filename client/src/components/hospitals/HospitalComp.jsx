9ec7039c6bac4f61136b13a89209cff8a63c3425
Joy Chen <joychen5069@gmail.com>
2020-08-13 11:28:23 -0400

    map no longer scrunched up

--- client/src/components/hospitals/HospitalComp.jsx
+++ client/src/components/hospitals/HospitalComp.jsx
@@ -7,29 +7,35 @@ import mapboxgl from 'mapbox-gl'
 //const env = dotenv.config().parsed;
 
 export default class HospitalsPage extends React.Component {
-//   state = {
-//     source: [],
-//     title: [],
-//     author: [],
-//     url: []
-//   }
-  
+    //   state = {
+    //     source: [],
+    //     title: [],
+    //     author: [],
+    //     url: []
+    //   }
+   
 
-  componentDidMount() {
+componentDidMount() {
     mapboxgl.accessToken = 'pk.eyJ1Ijoiam95Y2hlbjUwNjkiLCJhIjoiY2tkc3ZsMWp0MG1hMTJybnhpNW9wbDh3NyJ9.7jiZP4oIoTlXI--KIOjT5A'
+    
+    const mapDiv = document.getElementById('map');
+    if(mapDiv.style.visibility === true) map.resize();
 
     const map = new mapboxgl.Map({
         container: 'map',
         style: 'mapbox://styles/mapbox/streets-v11',
         center: [-77.04, 38.907],
-        });
-  };
+        zoom: 9
+    });
+};
 
-  render() {
+
+
+render() {
     return (
-      <>
-        <div id='map'>map</div>
-      </>
+        <>
+            <div id='map'>map</div>
+        </>
     )
-  }
+}
 }
