6c72159aa86639ea0035733431ea45cb8dcd6d9f
Joy Chen <joychen5069@gmail.com>
2020-08-13 12:33:14 -0400

    fixed map height bug

--- client/src/components/hospitals/HospitalComp.jsx
+++ client/src/components/hospitals/HospitalComp.jsx
@@ -1,57 +1,52 @@
 import React from 'react';
 import './hospital.css';
 import axios from "axios";
-import mapboxgl from 'mapbox-gl'
+import mapboxgl from 'mapbox-gl';
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
 
-//const dotenv = require('dotenv');
-//const env = dotenv.config().parsed;
 
 export default class HospitalsPage extends React.Component {
-    //   state = {
-    //     source: [],
-    //     title: [],
-    //     author: [],
-    //     url: []
-    //   }
-   
-
-componentDidMount() {
-    mapboxgl.accessToken = 'pk.eyJ1Ijoiam95Y2hlbjUwNjkiLCJhIjoiY2tkc3ZsMWp0MG1hMTJybnhpNW9wbDh3NyJ9.7jiZP4oIoTlXI--KIOjT5A'
     
-      function getLocation() {
-        if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(showPosition);
-        } 
-      }
-      
-      function showPosition(position) {
-          const userLat = position.coords.latitude.toFixed(2);
-          const userLon = position.coords.longitude.toFixed(2);
-        console.log(userLat, userLon) 
-      
+    componentDidMount() {
+        mapboxgl.accessToken = 'pk.eyJ1Ijoiam95Y2hlbjUwNjkiLCJhIjoiY2tkc3ZsMWp0MG1hMTJybnhpNW9wbDh3NyJ9.7jiZP4oIoTlXI--KIOjT5A'
 
- 
+        const getLocation = () => {
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(showPosition);
+            }
+        }
 
-    const map = new mapboxgl.Map({
-        container: 'map',
-        style: 'mapbox://styles/mapbox/streets-v11',
-        center: [userLon, userLat],
-        zoom: 10
-    });
-   
-}
-   getLocation()     
-};
+        const showPosition = (position) => {
+            const userLat = position.coords.latitude.toFixed(2);
+            const userLon = position.coords.longitude.toFixed(2);
+            console.log(userLat, userLon)
+
+            const map = new mapboxgl.Map({
+                container: 'map',
+                style: 'mapbox://styles/mapbox/streets-v11',
+                center: [userLon, userLat],
+                zoom: 10
+            });
+
+            map.addControl(
+                new MapboxGeocoder({
+                accessToken: mapboxgl.accessToken,
+                mapboxgl: mapboxgl
+                })
+                );
+        }
+        getLocation()
+    };
 
 
 
-render() {
-    return (
-        <>
-        <div className="col-9">
-            <div id='map'>map</div>
-            </div>
-        </>
-    )
-}
+    render() {
+        return (
+            <>
+                <div className="col">
+                    <div id='map'>map</div>
+                </div>
+            </>
+        )
+    }
 }
