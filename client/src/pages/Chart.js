e4c7743ca1b44be40991beedc06e3d4503f33540
Joy Chen <joychen5069@gmail.com>
2020-09-09 14:43:23 -0400

    Revert "Revert "adding chart component""
    This reverts commit 206c5b7f226bcee7c0073e4160b4f36145555604.

---
+++ client/src/pages/Chart.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Header from "../components/header/Header";
+import Footer from "../components/footer/Footer";
+import Navbar from "../components/navigation/Navbar";
+import Chart from "../components/chart/chart";
+import "../components/newsComponent/news.css";
+
+const ChartComp = (props) => {
+  return (
+    <>
+      <div className="container mainWrapper">
+        <Header />
+        <Navbar logout={props.logout} />
+
+        <h3 className="text-center pageTitle">Chart Placeholder</h3>
+
+        <div className="row newsContentWrapper">
+          <Chart />
+        </div>
+        <Footer />
+      </div>
+    </>
+  );
+};
+export default ChartComp;
