fea53590f45a6deb5000c5249b2c61e3c694c7e1
Joy Chen <joychen5069@gmail.com>
2020-09-17 10:06:50 -0400

    chart is showing up, just with incorrect data

--- client/src/components/CountryChart/countryChart.jsx
+++ client/src/components/CountryChart/countryChart.jsx
@@ -2,25 +2,25 @@ import React from 'react';
 import { Line } from 'react-chartjs-2';
 import axios from 'axios'
 
-
+const state = {
+    labels: ['test','test1'],
+    datasets: [
+        {
+            label: 'Active Cases',
+            fill: false,
+            lineTension: 0.5,
+            backgroundColor: 'rgba(75,192,192,1)',
+            borderColor: 'rgba(0,0,0,1)',
+            borderWidth: 2,
+            data: [12,34]
+        }
+    ]
+}
 
 
 export default class CountryChart extends React.Component {
 
-    state = {
-        labels: [],
-        datasets: [
-            {
-                label: 'Active Cases',
-                fill: false,
-                lineTension: 0.5,
-                backgroundColor: 'rgba(75,192,192,1)',
-                borderColor: 'rgba(0,0,0,1)',
-                borderWidth: 2,
-                data: []
-            }
-        ]
-    }
+    
     componentDidMount() {
 
         axios.get("https://api.covidtracking.com/v1/us/daily.json")
@@ -33,30 +33,26 @@ export default class CountryChart extends React.Component {
     };
 
     render() {
-        return (
-            <>
-                {this.state.data.map((data) => {
-                    return (
-                        <div className="col-9">
-                            <Line
-                                data={data.datasets.data}
-                                options={{
-                                    title: {
-                                        display: true,
-                                        text: 'United States Trend Chart',
-                                        fontSize: 20
-                                    },
-                                    legend: {
-                                        display: true,
-                                        position: 'right'
-                                    }
-                                }}
-                            />
-                        </div>
 
-                    );
-                })}
-            </>
-        )
+        return (
+            <div className="col-9">
+                <Line
+                    data={state}
+                    options={{
+                        title: {
+                            display: true,
+                            text: 'United States Trend Chart',
+                            fontSize: 20
+                        },
+                        legend: {
+                            display: true,
+                            position: 'right'
+                        }
+                    }}
+                />
+            </div>
+
+        );
+
     }
 }
