2f6cf9c9d1c5bbcbc29762f91b3fb8b408c754fc
Joy Chen <joychen5069@gmail.com>
2020-09-23 12:57:33 -0400

    finally selected data i wanted with dropdown

--- client/src/components/currentData/dropdown.jsx
+++ client/src/components/currentData/dropdown.jsx
@@ -1,26 +1,28 @@
+import { Event } from 'jquery';
 import React, { Component } from 'react';
 
 export default class StateDropdown extends Component {
     stateHandler = (event) => {
-        console.log("THIS IS BEING CALLED")
-        const stateSelected = event.value;
-        console.log(stateSelected)        
+        const stateSelected = event.target;
+        console.log(event.data);
+        console.log("state selected: ", stateSelected)
+        // this.props.stateSelected(stateSelected)          
     };
 
     render() {
         let stateList = ['Alabama','Alaska','Arizona','Arkansas','California','Colorado','Connecticut','Delaware','District of Columbia','Florida','Georgia','Guam','Hawaii','Idaho','Illinois','Indiana','Iowa','Kansas','Kentucky','Louisiana','Maine','Maryland','Massachusetts','Michigan','Minnesota','Mississippi','Missouri','Montana','Nebraska','Nevada','New Hampshire','New Jersey','New Mexico','New York','North Carolina','North Dakota','Ohio','Oklahoma','Oregon','Palau','Pennsylvania','Puerto Rico','Rhode Island','South Carolina','South Dakota','Tennessee','Texas','Utah','Vermont','Virgin Island','Virginia','Washington','West Virginia','Wisconsin','Wyoming'];
         let stateOptions = stateList.map((state, i) => (
-            <div value={state} key={i} onClick={this.stateHandler}>
+            <div value={state} key={i}>
                 {state}
             </div>
         ));
         return (
             <>
-                <div class="dropdown">
+                <div className="dropdown">
                     <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         Select State
                     </button>
-                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                    <div onClick={this.stateHandler} class="dropdown-menu" aria-labelledby="dropdownMenuButton">
                         {stateOptions}
                     </div>
                 </div>
--- client/src/components/currentData/stateData.jsx
+++ client/src/components/currentData/stateData.jsx
@@ -1,54 +1,89 @@
 import React from 'react';
-import StateDropdown from './dropdown';
+// import StateDropdown from './dropdown';
 // import './Current.css';
 import axios from "axios";
 
 export default class StateData extends React.Component {
-    date = new Date().getDate() - 1;
-    month = new Date().getMonth()+1;
-    year = new Date().getFullYear();
+  date = new Date().getDate() - 1;
+  month = new Date().getMonth() + 1;
+  year = new Date().getFullYear();
 
-    constructor() {
-        super();
-        this.state = {
-          value: "",
-          data: [],
-        };
-      }
+  constructor() {
+    super();
+    this.state = {
+      value: "",
+      data: [],
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-    async getState(value) {
-        // let stateSelected = this.props.state.province;
-        let res = await axios.get("https://api.covidtracking.com/v1/states/" + value + "/current.json");
-        console.log("value", value);
-        console.log(res.data)
-        this.setState({
-          data: res.data,
-          value: res.data.state
-        });
-      }
+//   stateHandler = (event) => {
+//     const stateSelected = event.target.value;
+//     console.log(event.data);
+//     console.log("state selected: ", stateSelected)
+//     // this.props.stateSelected(stateSelected)          
+// };
+handleChange(event) {
+  this.setState({ value: event.target.value });
+}
+handleSubmit = (event) => {
+  event.preventDefault();
+  this.getState(this.state.value);
+};
 
-      stateCall = (value) => {
-        this.setState({
-          value: value,
-        });
-        this.getState(value);
-        console.log(value)
-      };
+  async getState(value) {
+    // let stateSelected = this.value;
+    let res = await axios.get("https://api.covidtracking.com/v1/states/" + value + "/current.json");
+    
+    console.log(res.data)
+    this.setState({
+      data: res.data,
+      value: res.data.state
+    });
+  }
 
-    render() {
-        return (
-            <>
-            <StateDropdown value={this.state.value} />
-                <div id="test">
-                    <h5 className="dataTitle">{this.state.data.state} Data</h5>
-                    <p>Total Tests: {this.state.data.totalTestResults}</p>
-                    <p>Positive Cases: {this.state.data.positive}</p>
-                    <p>Negative Tests: {this.state.data.negative}</p>
-                    <p>Currently Hospitalized: {this.state.data.hospitalizedCurrently}</p>
-                    <p>Deaths: {this.state.data.death}</p>
-                </div>
-                <p className="disclosure">Disclosure: All data is sourced from The COVID Tracking Project and is up-to-date as of {this.month}/{this.date}/{this.year}. Please note that not all testing is reported and numbers may slightly vary from CDC data. </p>
-            </>
-        )
-    }
+  // stateCall = (value) => {
+  //   this.setState({
+  //     value: value,
+  //   });
+  //   this.getState(value);
+  //   console.log(value)
+  // };
+
+  render() {
+    let stateList = ['AL', 'AK', 'AS', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'DC', 'FM', 'FL', 'GA', 'GU', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MH', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'MP', 'OH', 'OK', 'OR', 'PW', 'PA', 'PR', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VI', 'VA', 'WA', 'WV', 'WI', 'WY'];
+    let stateOptions = stateList.map((state, i) => (
+      <option value={state} key={i}>
+        {state}
+      </option>
+    ));
+    return (
+      <>
+        <form onSubmit={this.handleSubmit} className="dropdownOptionForm">
+          <label>
+            <select value={this.state.value} onChange={this.handleChange}>
+              <option value="">Select a state</option>
+              {stateOptions}
+            </select>
+            <input
+              type="submit"
+              value="Click to view the data below"
+              className="stateSubmitBtn"
+            ></input>
+          </label>
+        </form>
+
+        <div id="stateData">
+          <h5 className="dataTitle">{this.state.data.state} Data</h5>
+          <p>Total Tests: {this.state.data.totalTestResults}</p>
+          <p>Positive Cases: {this.state.data.positive}</p>
+          <p>Negative Tests: {this.state.data.negative}</p>
+          <p>Currently Hospitalized: {this.state.data.hospitalizedCurrently}</p>
+          <p>Deaths: {this.state.data.death}</p>
+        </div>
+        <p className="disclosure">Disclosure: All data is sourced from The COVID Tracking Project and is up-to-date as of {this.month}/{this.date}/{this.year}. Please note that not all testing is reported and numbers may slightly vary from CDC data. </p>
+      </>
+    )
+  }
 }
